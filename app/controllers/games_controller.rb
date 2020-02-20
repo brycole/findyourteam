8ccae662f62c1a65621516848d9a60260a2b5d11
Rachid Khalidi rkhal@yahoo.fr <rkhal@yahoo.fr>
2020-02-20 19:47:36 +0000

    games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,23 @@
 class GamesController < ApplicationController
+    before_action :set_game
+
+  def index
+   @games = Game.all
+  end
+
+  def create
+   @game = Game.new(game_params)
+   @game.save
+  end
+
+private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(%i[title genre])
+  end
+
 end
