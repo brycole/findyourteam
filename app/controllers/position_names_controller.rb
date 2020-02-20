3253c1fa6438620690c98fd9a9aaa865defed4f6
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-02-20 21:04:09 +0000

    Added index / create / destroy to position names controller

--- app/controllers/position_names_controller.rb
+++ app/controllers/position_names_controller.rb
@@ -1,2 +1,27 @@
 class PositionNamesController < ApplicationController
+  before_action :set_game
+  def create
+    @position_name = PositionName.new(position_name_params)
+    @game = Game.find(params[:game_id])
+    @position_name.game = @game
+    @position_name.save
+  end
+
+  def index
+    @position_names = PositionName.all
+  end
+
+  def destroy
+    @position_name.delete
+  end
+
+  private
+
+  def set_game
+    @position_name = PositionName.find(params[:id])
+  end
+
+  def position_name_params
+    params.require(:position_name).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+games = [{ title: "Dota2", genre: "MOBA" }, { title: "CS: GO", genre: "FPS" }, { title: "League of Legends", genre: "MOBA" }]
+games.each do |game|
+  game = Game.new(game)
+  game.save
+end
+
+position_names_dota2 = ["Jungler", "Carry", "Solo", "Offlaner", "Support"]
+position_names_csgo = ["Fragger", "Leader", "AWPer", "Lurker", "Playmaker", "Secondary AWPer"]
+
+position_names_dota2.each do |position_name|
+  position_name = PositionName.new(name: position_name)
+  game = Game.find_by(title: "Dota2")
+  position_name.game = game
+  position_name.save
+end
+
+position_names_csgo.each do |position_name|
+  position_name = PositionName.new(name: position_name)
+  game = Game.find_by(title: "CS: GO")
+  position_name.game = game
+  position_name.save
+end
