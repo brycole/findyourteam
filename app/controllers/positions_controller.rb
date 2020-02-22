c4e0bd0421b4d2c22ef84f46ff4581c3177af4f2
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-02-22 12:58:44 +0000

    Added positions create / index / delete for a team

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,2 +1,36 @@
 class PositionsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @team = Team.find(params[:team_id])
+    @positions = Position.all
+  end
+
+  def new
+    @team = Team.find(params[:team_id])
+    @position_names = PositionName.where(game_id: @team.game)
+    @position = Position.new
+  end
+
+  def create
+    @position = Position.new
+    @team = Team.find(params[:team_id])
+    @position.team = @team
+    @position_name = PositionName.find(params[:position][:position_name])
+    @position.position_name = @position_name
+    @position.save
+
+    redirect_to team_positions_path(@team.id)
+  end
+
+  def edit
+    @position = Position.find()
+  end
+
+  def destroy
+    @position = Position.find(params[:id])
+    @position.delete
+
+    redirect_to team_positions_path(params[:team_id])
+  end
 end
