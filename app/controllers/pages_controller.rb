9bdcb5d82e95ed7a10fc1b230a7091b23892b18b
Bryan Cole <brycole@gmail.com>
2020-02-22 13:59:02 +0000

    added video to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,12 @@ class PagesController < ApplicationController
   def home
     lol = "lol_group_battle.mp4"
     dota2 = "dota2_blackhole.mp4"
-    @video = lol
 
+    @video = dota2
+
+    if current_user.try(:show_game) == "lol"
+      @video = lol
+    end
     #config.assets.paths << "#{Rails.root}/app/assets/videos"
     # <video autoplay="autoplay" loop="loop" muted="muted" src="<%= @video %>"></video>
     # <%= video_tag "dota2_blackhole.mp4" %>
