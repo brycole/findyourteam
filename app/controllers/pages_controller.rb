05d49455aeb2364e623034ac5276c9309da125fd
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-02-22 16:44:20 +0000

    Added three different game videos/buttons to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,10 @@ class PagesController < ApplicationController
   def home
     lol = "lol_group_battle.mp4"
     dota2 = "dota2_blackhole.mp4"
+    cs = "HatefulHauntingAngora.webm"
 
     @video = dota2
+    @video2 = lol
 
     if current_user.try(:show_game) == "lol"
       @video = lol
@@ -14,5 +16,4 @@ class PagesController < ApplicationController
     # <video autoplay="autoplay" loop="loop" muted="muted" src="<%= @video %>"></video>
     # <%= video_tag "dota2_blackhole.mp4" %>
   end
-
 end
