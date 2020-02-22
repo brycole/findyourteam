f7b24678975f9de4fb9810f8621bcea92857970f
Rachid Khalidi rkhal@yahoo.fr <rkhal@yahoo.fr>
2020-02-22 11:35:51 +0000

    migration nickname and rank

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_02_18_212606) do
-
+ActiveRecord::Schema.define(version: 2020_02_22_112653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_212606) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
