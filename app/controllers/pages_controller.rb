7b65db0a1d1945e60fef2d556156b72564379014
Bryan Cole <brycole@gmail.com>
2020-02-22 12:57:38 +0000

    videos added and swappable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    lol = "lol_group_battle.mp4"
+    dota2 = "dota2_blackhole.mp4"
+    @video = lol
+
+    #config.assets.paths << "#{Rails.root}/app/assets/videos"
+    # <video autoplay="autoplay" loop="loop" muted="muted" src="<%= @video %>"></video>
+    # <%= video_tag "dota2_blackhole.mp4" %>
   end
+
 end
