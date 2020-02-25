8d7e99fded64111e1cf0214f3c46c13851d634be
Bryan Cole <brycole@gmail.com>
2020-02-25 20:22:46 +0000

    scheme change

--- db/schema.rb
+++ db/schema.rb
@@ -102,7 +102,6 @@ ActiveRecord::Schema.define(version: 2020_02_22_131708) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
     t.string "show_game"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
